d3bd1bb915171a03cbc497ef7eff0888c4059fb5
Darvin Rio <todarvinrio@gmail.com>
2021-06-11 12:07:50 +0530

    fixed getIt()

--- calculate.py
+++ calculate.py
@@ -1,8 +1,8 @@
 import getPrice as gp
 import ban 
 
-def calculate(coin, no): # return value of total coins in usd and inr
+def calculate(coin, no): # return value of total coins in usd and inr as dictionary
     coin_price = gp.getPrice(coin)
     usd_val = coin_price['usd'] * no
     inr_val = coin_price['inr'] * no
-    return [usd_val, inr_val]
+    return {'usd' : usd_val, 'inr' : inr_val}
--- main.py
+++ main.py
@@ -8,32 +8,33 @@ import asyncio
 
 wallets = pd.read_csv('private/wallets.csv')
 
-wallets['coins'] = 0
-wallets['usd_value'] = 0
-wallets['inr_value'] = 0
-
-print(wallets.head())
-
 def getIt(row):
+    
+    # setup output dict
+    output = {
+        'coin' : 0,
+        'usd_value' : 0,
+        'inr_value' : 0 
+    }
     # get row label
     l = row[0] 
-
+  
     # get wallet balance
-    wallets.iloc[l,3] = ban.getBalance(wallets.iloc[l,2])
+    output['coin'] = ban.getBalance(wallets.iloc[l,2])
 
     # calculate value of coins
-    value = calculate.calculate(wallets.iloc[l,1],wallets.iloc[l,3])
+    value = calculate.calculate(wallets.iloc[l,1],output['coin'])
 
     print(value)
 
     #store 
-    wallets.iloc[l,4] = value[0]
-    wallets.iloc[l,5] = value[1]
-    print(wallets.head())
+    output['usd_value'] = value['usd']
+    output['inr_value'] = value['inr']
+    
+    return output
 
 cpu = mp.cpu_count()
 
 with mp.Pool(cpu) as pool :
     pool.map(getIt, wallets.itertuples(name=None))
 
-print(wallets.head())

1c47783277ff4831dc04581f97287bff7e818f3b
Darvin Rio <todarvinrio@gmail.com>
2021-06-14 15:35:20 +0530

    ban completed

--- main.py
+++ main.py
@@ -10,31 +10,47 @@ wallets = pd.read_csv('private/wallets.csv')
 
 def getIt(row):
     
+    # get row label
+    l = row[0] 
+
     # setup output dict
-    output = {
-        'coin' : 0,
+    outDict = {
+        'wallet_name' : row[1], # store wallet name
+        'coin' : row[2],        # store coin name
+        'address': row[3],      # store wallet address
+        'coin_no' : 0,
         'usd_value' : 0,
         'inr_value' : 0 
     }
-    # get row label
-    l = row[0] 
-  
+     
     # get wallet balance
-    output['coin'] = ban.getBalance(wallets.iloc[l,2])
+    outDict['coin_no'] = ban.getBalance(outDict['address'])
 
     # calculate value of coins
-    value = calculate.calculate(wallets.iloc[l,1],output['coin'])
-
-    print(value)
+    value = calculate.calculate(wallets.iloc[l,1],outDict['coin_no'])
 
     #store 
-    output['usd_value'] = value['usd']
-    output['inr_value'] = value['inr']
+    outDict['usd_value'] = value['usd']
+    outDict['inr_value'] = value['inr']
     
-    return output
+    return outDict
 
 cpu = mp.cpu_count()
 
-with mp.Pool(cpu) as pool :
-    pool.map(getIt, wallets.itertuples(name=None))
+#with mp.Pool(cpu) as pool :
+#    x = pool.map(getIt, wallets.itertuples(name=None))
+
+def printOut():
+
+    # call multiprocess for each row and get List of Dicts
+    with mp.Pool(cpu) as pool :
+        outDicts = pool.map(getIt, wallets.itertuples(name=None))
+
+    #convert List of Dicts to DF
+    d = pd.DataFrame(outDicts)
+    
+    print(d)
+    
+
+printOut()
 

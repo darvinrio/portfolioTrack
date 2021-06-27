4aa297410c58fc7b66078f3a464ce7798562bfbc
Darvin Rio <todarvinrio@gmail.com>
2021-06-28 00:09:48 +0530

    json cleaning and code reusability

--- luna.py
+++ luna.py
@@ -12,13 +12,41 @@ walletJson = {
 } 
 
 terra = LCDClient(chain_id="columbus-4", url="https://lcd.terra.dev")
+lunaPrice = price.getPrice('luna')
+
+def getCoinValBase(denom):
+    exchangeRate = (terra.oracle.exchange_rates()).get(denom)
+    exchangeRateDec = ((exchangeRate.to_data())['amount'])
+
+    baseVal = 1/float(exchangeRateDec)
+    return baseVal
 
 
-def getCoinBal(wallet, coin):
-    bal = wallet
-    coinBal = bal.get(coin).to_data()['amount']
-    properCoin = float(coinBal)/(10**6) 
-    return properCoin
+def getCoinBal(Coin, coinDenom, coinType='token'): #coinType = base for baseToken
+
+    coin = coinName(coinDenom)
+    token = cj.createJSON(coinType)
+
+    coinBalInt = Coin.to_data()['amount']
+    coinBal = float(coinBalInt)/(10**6)
+    coinPrice = price.getPrice(coin)
+    value = calculate.calculateVal(coinPrice, coinBal)
+
+    token[coinType] = coin
+    token['noOfCoins'] = coinBal
+    token['usd_value'] = coinPrice['usd']
+    token['total_usd_value'] = value['usd']
+    token['total_inr_value'] = value['inr']
+
+    if(coinType == 'base'):
+        return token
+    else:
+        baseVal = getCoinValBase(coinDenom)
+        token['base_value'] = baseVal
+        token['total_base_value'] = coinBal * baseVal
+
+    return token
+
 
 def coinName(coin): #you might have to replace this with dictionary
     switch = {
@@ -27,10 +55,11 @@ def coinName(coin): #you might have to replace this with dictionary
     }
     return switch.get(coin)
 
+
 def getLunaWallet(wallet):
     walletBank = terra.bank.balance(wallet['address'])
     coinList = walletBank.denoms()
-    lunaJson =  cj.createBaseJSON()
+    lunaJson =  cj.createJSON()
 
     lunaJson['walletName'] = wallet['name']
     lunaJson['network'] = 'luna'
@@ -43,37 +72,69 @@ def getLunaWallet(wallet):
     i = 0
 
     for coinDenom in coinList:
-        coin = coinName(coinDenom)
+        #coin = coinName(coinDenom)
+        coin = walletBank.get(coinDenom)
 
         if coinDenom != 'uluna':
-            token = cj.createTokenJSON()
+            token = getCoinBal(coin, coinDenom)
             lunaJson['tokens'].append(token)
-                     
-            lunaJson['tokens'][i]['token'] = coin
-            coinBal = getCoinBal(walletBank, coinDenom)
-            lunaJson['tokens'][i]['noOfCoins'] = coinBal
-            coinPrice = price.getPrice(coin)
-            value = calculate.calculateVal(coinPrice, coinBal)
-            lunaJson['tokens'][i]['usd_value'] = coinPrice['usd']
-            lunaJson['tokens'][i]['total_usd_value'] = value['usd']
-            lunaJson['tokens'][i]['total_inr_value'] = value['inr']
-
-            i += 1
+            
             continue
 
-        lunaJson['base']['base'] = coin
-        coinBal = getCoinBal(walletBank, coinDenom)
-        lunaJson['base']['noOfCoins'] = coinBal
-        coinPrice = price.getPrice(coin)
-        value = calculate.calculateVal(coinPrice, coinBal)
-        lunaJson['base']['usd_value'] = coinPrice['usd']
-        lunaJson['base']['total_usd_value'] = value['usd']
-        lunaJson['base']['total_inr_value'] = value['inr']
+        base = getCoinBal(coin, coinDenom, 'base')
+        lunaJson['base']= base    
+        
       
     return lunaJson
 
+
+def lunaStaking(wallet):
+    lunaStake = terra.staking.delegations(delegator = wallet['address'])
+    lunaRew = terra.distribution.rewards(wallet['address'])
+
+    stake = cj.createJSON('stake')
+    stake['token'] = 'luna'
+
+    delegations = []
+
+    for delegation in lunaStake:
+        stakeTemp = cj.createJSON('delegation')
+
+        val = terra.staking.validator(delegation.validator_address)
+        stakeTemp['validator'] = (val.description.moniker)
+        stakeTemp['validatorAddress'] = (delegation.validator_address)
+
+        noOfStakedCoins = delegation.balance.to_data()['amount']
+        coinBal = stakeTemp["noOfStakedCoins"] = float(noOfStakedCoins)/(10**6)
+        stakeTemp['usd_value'] = lunaPrice
+        value = calculate.calculateVal(lunaPrice, coinBal)
+        stakeTemp['total_usd_value'] = value['usd']
+        stakeTemp['total_inr_value'] = value['inr']
+
+        rewardCoins = lunaRew.rewards[delegation.validator_address]
+        coinList = rewardCoins.to_data()
+        print(coinList)
+
+        
+        delegations.append(stakeTemp)
+
+    stake['delegations'] = delegations
+    return stake
+
+
+def getStakingReward(wallet):
+    lunaRew = terra.distribution.rewards(wallet['address'])
+    coins = lunaRew.rewards
+    print(coins)
+    #coinList = coins.denoms()
+    #return coinList
+
+
 if __name__ == "__main__" :
-    out = getLunaWallet(walletJson)
-    print(json.dumps(out))
-    with open('jsonFormats/test.json','w') as file :
-        file.write(json.dumps(out))
+    var = getLunaWallet(walletJson)
+    # for dele in var:
+    #     var2 = var[dele]
+    #     print(var2)
+    #     print(type(var2))
+    print(var)
+    # print(type(var))
